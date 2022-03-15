2d32e1a9c0437689627a0bc1f9c4187ae34bf019
I426076 <i426076@fhict.nl>
2022-03-15 15:13:25 +0100

    fixed API fetch

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ let people = [];
 fetch("https://randomuser.me/api/?results=5")
   .then((response) => response.json())
   .then((data) => {
-    people.push(data.results);
+    people.push(...data.results);
+    const myElement = document.getElementById("people");
+
+    for (let i = 1; i < myElement.children.length; i++) {
+      myElement.children[i].children[0].innerHTML =
+        people[i - 1].name.first + " " + people[i - 1].name.last;
+      myElement.children[i].children[1].src = people[i - 1].picture.large;
+    }
   });
-
-console.log(people);
-
-const myElement = document.getElementById("people");
-
-for (let i = 1; i < myElement.children.length; i++) {
-  myElement.children[i].children[0].innerHTML = people[i - 1].name.first;
-}
